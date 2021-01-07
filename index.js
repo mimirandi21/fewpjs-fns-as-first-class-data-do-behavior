f7669309fe6aece72236d50bbf80ddf0107bd132
Randi Schreiner <mimirandi21@gmail.com>
2021-01-07 14:55:20 -0600

    commit tree 194fea01bd520c89a072525a15ae2b3f5294f420

--- index.js
+++ index.js
@@ -3,11 +3,26 @@
 document.querySelector("button").addEventListener("click", handleClick);
 
 function handleClick(e) {
-  const timeString = document.getElementById('time').value
-  displayMessage(greet(timeString))
+	const timeString = document.getElementById("time").value;
+	displayMessage(greet(timeString));
 }
 
 /* End Given Code, don't edit above here...*/
 
 /* Write your implementation of greet() */
+
+function greet(time) {
+	time = parseInt(time);
+	if (time < 12) {
+		return "Good Morning";
+	} else if (time >= 12 && time <= 17) {
+		return "Good Afternoon";
+	} else {
+		return "Good Evening";
+	}
+}
 /* Write your implementation of displayMessage() */
+function displayMessage(text) {
+	let greeting = document.getElementById("greeting");
+	greeting.innerText = text;
+}
